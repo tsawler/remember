60fba3ae75d67bbb86066b0d2e171616f8cfe929
Trevor Sawler <trevor.sawler@gmail.com>
2024-05-02 13:42:01 -0300

    in progress

--- cache.go
+++ cache.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/redis/go-redis/v9"
+	"strconv"
 	"time"
 )
 
@@ -24,15 +25,15 @@ func New(server, port, password string, db int) *Cache {
 	return &Cache{Client: client}
 }
 
-// UnmarshalBinary takes a value retrieved from the cache, which will be a JSON string,
-// and unmarshals it into value, which must be a pointer. Any non-scalar type we want
+// UnmarshalBinary takes a value retrieved from the cache, which will be a JSON string.
+// We unmarshal it into value, which must be a pointer. Any non-scalar type we want
 // to store in the cache must implement the MarshalBinary method, i.e.:
 //
 //	 func (m Student) MarshalBinary() ([]byte, error) {
 //		  return json.Marshal(m)
 //	 }
-func (c *Cache) UnmarshalBinary(data []byte, value any) error {
-	return json.Unmarshal(data, value)
+func (c *Cache) UnmarshalBinary(data string, value any) error {
+	return json.Unmarshal([]byte(data), value)
 }
 
 // Set puts a value into Redis. The final parameter, expires, is optional.
@@ -56,6 +57,57 @@ func (c *Cache) Get(key string) (any, error) {
 	}
 
 	return val, nil
+}
+
+// GetInt to retrieve a value from the cache, convert it to an int, and return it.
+func (c *Cache) GetInt(key string) (int, error) {
+	ctx := context.Background()
+
+	val, err := c.Client.Get(ctx, key).Result()
+	if err != nil {
+		return 0, err
+	}
+
+	i, err := strconv.Atoi(val)
+	if err != nil {
+		return 0, err
+	}
+
+	return i, nil
+}
+
+// GetFloat64 to retrieve a value from the cache, convert it to an float64, and return it.
+func (c *Cache) GetFloat64(key string) (float64, error) {
+	ctx := context.Background()
+
+	val, err := c.Client.Get(ctx, key).Result()
+	if err != nil {
+		return 0, err
+	}
+
+	i, err := strconv.ParseFloat(val, 64)
+	if err != nil {
+		return 0, err
+	}
+
+	return i, nil
+}
+
+// GetFloat32 to retrieve a value from the cache, convert it to an float32, and return it.
+func (c *Cache) GetFloat32(key string) (float64, error) {
+	ctx := context.Background()
+
+	val, err := c.Client.Get(ctx, key).Result()
+	if err != nil {
+		return 0, err
+	}
+
+	i, err := strconv.ParseFloat(val, 32)
+	if err != nil {
+		return 0, err
+	}
+
+	return i, nil
 }
 
 // Delete removes an item from the cache, by key.
