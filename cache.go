8f4b1fd5c65542a2aa2c75a047996ee97c72513a
Trevor Sawler <trevor.sawler@gmail.com>
2024-05-02 11:24:17 -0300

    in progress

--- cache.go
+++ cache.go
@@ -1,1 +1,15 @@
 package cache
+
+import (
+	"fmt"
+	"github.com/redis/go-redis/v9"
+)
+
+func New(server, port, password string, db int) *redis.Client {
+	client := redis.NewClient(&redis.Options{
+		Addr:     fmt.Sprintf("%s:%s", server, port),
+		Password: password,
+	})
+
+	return client
+}
